4ed2c062ca49f0e87eab0eaa1d03611f5b57cff0
Rachel Lewis <rachel.e.lewis@proton.me>
2025-12-14 17:16:44 -0800

    Refactor landing page to use class component with dynamic rendering of LoadPortfolio and Landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
-import Landing from '@/components/Dumb/welcome-page';
+"use client";
 
-export default function LandingPage() {
-  return (
-    <Landing />
-  );
+import {Landing, LoadPortfolio} from '@/components/Dumb/welcome-page';
+import {Component} from 'react';
+
+interface HomePageState {
+  formInput: boolean;
+}
+
+export default class HomePage extends Component<any, HomePageState> {
+  constructor(props: any){
+    super(props);
+    this.state = {formInput: false,};
+  }
+
+  handleClick = () => {
+    this.setState((prevState) => ({formInput: !prevState.formInput}));
+  };
+
+  renderComponent = () => {
+    switch (this.state.formInput) {
+      case true:
+        return <LoadPortfolio onClick={this.handleClick} />;
+      default:
+        return <Landing onClick={this.handleClick} />;
+    }
+  };
+
+  render() {
+    return (this.renderComponent());
+  }
 }
